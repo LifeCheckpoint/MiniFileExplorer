c187d1710d3252d91e734227b0b76b3334b52f0c
LifeCheckpoint <lifechpt@qq.com>
2025-11-28 21:20:52 +0800

    修改models时间类型错误

--- src/models/include/models.h
+++ src/models/include/models.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdint>
 #include <chrono>
+#include <filesystem>
 
 // 文件类型枚举
 enum class FileType {
@@ -13,9 +14,9 @@ enum class FileType {
 
 // 单个文件或文件夹的详细信息
 struct FileInfo {
-    std::string name;       // 文件名
-    std::string path;       // 绝对路径
-    FileType type;          // 类型
-    uintmax_t size;         // 大小 (字节)，文件夹为 0
-    std::chrono::time_point modifyTime; // 修改时间
+    std::string name;                           // 文件名
+    std::string path;                           // 绝对路径
+    FileType type;                              // 类型
+    uintmax_t size;                             // 大小 (字节)，文件夹为 0
+    std::filesystem::file_time_type modifyTime; // 修改时间
 };
